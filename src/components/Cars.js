2803244d697399f1d9b6be31c0571ec807e392f2
indusfiretiger <indusfiretiger@gmail.com>
2019-03-14 20:13:08 +1100

    rename variables and functions, add code to make the API request with comments to expain its error

--- src/components/Cars.js
+++ src/components/Cars.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import shows from '../static/data.json'
+import shows from '../static/data.json' // read a local json file instead of API
 
-const Cars = (props) => {
+const Cars = (props) => { 
+  let displayCars = []
   const getModelDetails = (brand, name) => {
     for (let i = 0; i < brand.models.length; i++) {
       if (brand["models"][i].name === name) {
@@ -18,19 +19,8 @@ const Cars = (props) => {
     }
     return null
   }
-
-  const processList = () => {
-    let cars = []
-    shows.forEach(show => {
-      for (let i = 0; i < show.cars.length; i++) {
-        let car = {
-          make: show.cars[i].make,
-          model: show.cars[i].model,
-          show: show.name
-        }
-        cars.push(car)
-      }
-    })
+  
+  const createBrands = (cars) => {
     let brands = []
     cars.forEach(car => {
       let name = car.make
@@ -50,6 +40,22 @@ const Cars = (props) => {
         }
       }
     })
+    return brands
+  }
+
+  const getCarsFromShows = () => {
+    let cars = []
+    shows.forEach(show => {
+      for (let i = 0; i < show.cars.length; i++) {
+        let car = {
+          make: show.cars[i].make,
+          model: show.cars[i].model,
+          show: show.name
+        }
+        cars.push(car)
+      }
+    })
+    let brands = createBrands(cars)
     brands.sort((a, b) => {
       if (a.make < b.make) { return -1; }
       if (a.make > b.make) { return 1; }
@@ -57,27 +63,61 @@ const Cars = (props) => {
     })
     return brands
   }
-  let cars = processList()
-  let rows = []
-  let makeIdx = 0
-  let modelIdx = 0
-  let showIdx = 0
-  cars.forEach(car => {
-    rows.push(<p className ='level1' key={'make'+makeIdx}>Make: {car.make}</p>);
-    car.models.forEach(model => {
-      rows.push(<p className ='level2' key={'model'+modelIdx}>model: {model.name}</p>);
-      modelIdx++
-      model.shows.forEach(show => {
-        rows.push(<p className ='level3' key={'show'+showIdx}>show: {show}</p>);
-        showIdx++
+
+  function makeCarDisplayList() {
+    let cars = getCarsFromShows()
+    let displayCars = []
+    let makeIdx = 0
+    let modelIdx = 0
+    let showIdx = 0
+    cars.forEach(car => {
+      displayCars.push(<p className ='level1' key={'make'+makeIdx}>Make: {car.make}</p>);
+      car.models.forEach(model => {
+        displayCars.push(<p className ='level2' key={'model'+modelIdx}>model: {model.name}</p>);
+        modelIdx++
+        model.shows.forEach(show => {
+          displayCars.push(<p className ='level3' key={'show'+showIdx}>show: {show}</p>);
+          showIdx++
+        })
       })
+      makeIdx++
     })
-    makeIdx++
-  })
+    return displayCars
+  }
+  function fetchData() {
+    let url =
+      "http://eacodingtest.digital.energyaustralia.com.au/api/v1/cars"
+    let options = {
+      method: "GET",
+      // mode: "no-cors", // setting no-cors header results in an opaque response in which the data is not readable
+      headers: {
+        "Content-Type": "text/plain"
+      }
+    }
+    fetch(url, options)
+      .then(response => {
+        console.log(response) 
+        // shows = response.json() // ideally response from the API to populate the shows here
+        // instead read a local json file
+      })
+      .catch(function(error) {
+        console.log(error)
+        // logs the following error
+        /* -------------------------------------------------------------------------------------
+        Access to fetch at 'http://eacodingtest.digital.energyaustralia.com.au/api/v1/cars' 
+        from origin 'http://localhost:3000' has been blocked by CORS policy: 
+        No 'Access-Control-Allow-Origin' header is present on the requested resource. 
+        If an opaque response serves your needs, set the request's mode to 'no-cors' to fetch 
+        the resource with CORS disabled.       
+        -------------------------------------------------------------------------------------*/
+      })
+  }
+  // fetchData()  // requests swagger API to get the data to display
+  displayCars = makeCarDisplayList() 
   return (
     <div className = "cars">
       <h2>Cars</h2>
-      {rows}
+      {displayCars}
     </div>
   );
 }
